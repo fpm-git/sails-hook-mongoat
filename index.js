ce448bc11c124baa2f1457b90382721fea429735
Yuki von Kanel <poison@ruadh.io>
2018-04-05 13:04:36 -0500

    Ample cleanup and modernisation
    The hook has been upgraded to take advantage of the latest ES features,
    making things quite a bit cleaner.
    
    Some barebones documentation has also been added, and the flow of logic
    has hopefully been streamlined in an effort to make the code simpler to
    understand and maintain.
    
    The unused `validProperties` array has been removed as there is no need
    to perform validation from the hook: Mongo will do this for us.
    
    Rather than choosing either Sails v1.0 or the still widely used v0.12.*
    to fail on, support now exists for both.
    
    Async and lodash are no longer needed/used.

--- index.js
+++ index.js
@@ -1,103 +1,92 @@
 /**
- * Created by Mike on 26/05/15.
+ * sails-hook-mongoat
+ *
+ * A hook for Sails.js providing advanced Mongo indexing options for models making use of
+ * the `sails-mongo` adapter. Supports Sails v1 and v0.12.
+ *
+ * Originally created by Mike Diarmid (https://github.com/Salakar) on 26-05-2015.
+ * Fork maintained by Floatplane Media (https://github.com/fpm-git).
  */
-module.exports = function (sails) {
-  // store created indexes
-  var indexes         = [],
-      // see http://docs.mongodb.org/manual/reference/method/db.collection.createIndex/#options-for-all-index-types
-      // UNUSED  - TODO check for valid options.
-      validProperties = [
-        'unique', // boolean
-        'name',   // string
-        'expiresAfterSeconds', // integer
-        'sparse', // boolean
-        'background', // boolean
-        // text index options
-        'weights', // object
-        'default_language', // string
-        'language_override', // string
-        'textIndexVersion',  // integer
-        // options for for 2dsphere Indexes,
-        '2dsphereIndexVersion', // integer
-        // options for 2d indexes
-        'bits', // integer
-        'min',  // number
-        'max',  // number
-        // options for geoHaystack Indexes
-        'bucketSize' // number
-      ];
 
-  if (!_)
-    var _ = require('lodash');
+const SailsHookMongoat = (sails) => ({
 
-  if (!async)
-    var async = require('async');
+  initialize(done) {
+    sails.after('hook:orm:loaded', () => {
+      this.createAllIndexes().then(res => done()).catch(done);
+    });
+  },
 
+  /**
+   * Handles creating all indices, simply by iterating over all models and calling the
+   * createModelIndexes method for each.
+   */
+  async createAllIndexes() {
+    sails.log.debug('Mongoat: creating model indexes...');
 
-  var getIndexes = function (key, done) {
-    var model = sails.models[key];
-    // check for indexes
-    if (_.isArray(model.indexes) && model.indexes.length > 0) {
-      async.forEachOf(model.indexes, function (indexObject, i, done) {
-        model.indexes[i].model = key; // add model name to index
-        done();
-      }, function () {
-        indexes = _.union(indexes,model.indexes);
-        done();
-      });
-    } else {
-      done();
+    // leave without applying indices, if we're not using an 'alter' or 'drop' strategy
+    if (sails.config.models && (['alter', 'drop'].indexOf(sails.config.models.migrate) < 0)) {
+      sails.log.warn(`Mongoat: not creating indexes due to model migration strategy "${sails.config.models.migrate}". Use "alter" or "drop" to enable index creation, or build them manually.`);
+      return;
     }
-  };
 
-  return {
-    createIndex: function (modelName, fields, options, next) {
-      var model = sails.models[modelName];
-      // check model adapter is sails-mongo by checking first connections adapter -- is this the best way?
-      if (model && model._adapter.datastores[Object.keys(model._adapter.datastores)[0]].config.adapter == 'sails-mongo')
-        model.getDatastore().manager.collection(model.tableName).ensureIndex(fields, options, function (err) {
-          if (err) {
-            sails.log.error('Mongoat: Error creating index for model', modelName);
-            sails.log.error(fields);
-            sails.log.error(err);
-          }
-          else
-            sails.log.verbose('Mongoat: An index was created for model', modelName);
+    // load indices for each model. we wait for each "synchronously," but Mongo doesn't parallelise it anyway (by default)... (and we wouldn't want to break guarantees by doing that either)
+    for (modelName in sails.models) {
+      await this.createModelIndexes(sails.models[modelName]);
+    }
+  },
 
-          if (_.isFunction(next))
-            next(err);
+  /**
+   * Handles creating the indices for a single model.
+   *
+   * @param model - A sails model definition, as obtained from sails.models.
+   */
+  async createModelIndexes(model) {
+    // leave early if this model has no custom indexes specified
+    if (!Array.isArray(model.indexes) || (model.indexes.length === 0)) {
+      return;
+    }
 
-        });
-      else {
-        if (_.isFunction(next))
-          next('Model not provided or model adapter is not sails-mongo.')
+    // grab our model connections/datastores, so we can make sure this model is on Mongo (if not, leave with a warning)
+    const connections = model.adapter ? model.adapter.connections : model._adapter.datastores;  // v1 and v0.12 support
+    if (connections[Object.keys(connections)[0]].config.adapter !== 'sails-mongo') {
+      sails.log.warn(`Mongoat: skipping index creation for model "${model.tableName}" as it is not using the "sails-mongo" adapter.`);
+      return;
+    }
+
+    // create a Mongo index for each model-defined index item
+    const collection = (typeof model.getDatastore === 'function') ? model.getDatastore().manager.collection(model.tableName) : await asyncify(model.native);
+    model.indexes.forEach(async index => {
+      try {
+        await collection.ensureIndex(index.attributes, index.options || {});
+        sails.log.verbose(`Mongoat: created index for model "${model.tableName}" !`);
+      } catch (e) {
+        sails.log.error(`Mongoat: failed to create index for model "${model.tableName}":`, e);
       }
-    },
-    initialize: function (cb) {
-      var self = this;
-      var eventsToWaitFor = [];
+    });
+  },
 
-      if (sails.hooks.orm)
-        eventsToWaitFor.push('hook:orm:loaded');
+});
 
-      sails.after(eventsToWaitFor, function () {
-        sails.log.verbose('sails mongoat started');
+/**
+ * Helper function used to call a callback-accepting function, returning the result as a
+ * promise instead, for easy usage with async/await.
+ *
+ * @param {Function} func - The function which should be executed. Expected to accept a
+ * callback of the form (err, ...results).
+ * @param {...any} params - A list of parameters to pass through to `func`.
+ *
+ * @returns {Promise<any|any[]>} a promise which resolves to either a single value, or an
+ * array representing multiple values returned, based on the callback response.
+ */
+function asyncify(func, ...params) {
+  return new Promise((resolve, reject) => {
+    func(...params, (err, ...res) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(res.length <= 1 ? res[0] : res);
+    });
+  });
+}
 
-        if(sails.config.models.migrate !== 'alter' && sails.config.models.migrate !== 'drop') {
-          sails.log.verbose('sails mongoat skipping index creation (according to "' + sails.config.models.migrate + '" migration strategy)');
-          return cb();
-        }
-
-        async.each(Object.keys(sails.models), getIndexes, function () {
-          async.each(indexes, function (index, done) {
-            self.createIndex(index.model, index.attributes, index.options || {}, done);
-          }, function () {
-            cb();
-          });
-        });
-      });
-
-
-    }
-  };
-};
+module.exports = SailsHookMongoat;
